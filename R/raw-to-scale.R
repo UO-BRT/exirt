c901dbb91d4fafc96d208ea7a30dfea135cf832a
Daniel Anderson <daniela@uoregon.edu>
2022-01-06 16:45:46 -0800

    add raw to rit function

--- R/raw-to-scale.R
+++ R/raw-to-scale.R
@@ -2,7 +2,7 @@ raw_to_scale <- function() {
   items <- db_get("Items")
   pd <- get_pattern_data(items = items)
 
-  models <- lapply(pd, rasch, items)
+  models <- lapply(pd, rasch_, items)
 
   d <- lapply(pd, function(x) {
     x$id <- rownames(x)
@@ -11,7 +11,7 @@ raw_to_scale <- function() {
 
   thetas <- Map(estimate_theta, models, d)
   raw_theta <- lapply(thetas, function(x) {
-    x$raw <- rowSums(x[ ,seq_len(ncol(x) - 3)])
+    x$raw <- rowSums(x[, seq_len(ncol(x) - 3)])
     x[c("raw", "theta", "theta_se")]
   })
 
@@ -25,14 +25,14 @@ pull_cuts <- function(name) {
 }
 
 convert_theta <- function(raw_theta_tbl, name, round = TRUE) {
-  cut <- pull_cuts(name)
+  cuts <- pull_cuts(name)
 
-  ss <- (raw_theta_tbl$theta * 10) + cut$add
+  ss <- (raw_theta_tbl$theta * 10) + cuts$add
   if (round) {
     ss <- round2(ss)
   }
   pl <- cut(ss,
-            breaks = c(-Inf, cut$c1, cut$c2, cut$c3, Inf),
+            breaks = c(-Inf, cuts$c1, cuts$c2, cuts$c3, Inf),
             labels = 1:4)
 
   d <- data.frame(
@@ -46,5 +46,20 @@ convert_theta <- function(raw_theta_tbl, name, round = TRUE) {
   cbind.data.frame(raw_theta_ss, d)
 }
 
-# rs <- raw_to_scale()
-# mapping <- lapply(rs, convert_theta, names(rs))
+#' Raw to RIT conversions
+#' 
+#' Creates a single dataframe that has the raw score, theta value and standard
+#' error associated with that raw score, the conversion to a RIT score and
+#' RIT standard error, and the performance level for the corresponding score.
+#' All conversions are returned for all tests.
+#'
+#' @param round Should the RIT scores be rounded? Defaults to \code{TRUE}. Note
+#'   the rounding is done as typical, not as [base::round()] does. See the 
+#'   source code for the \code{round2} function for more detail.
+#' @export
+#' 
+raw_to_rit <- function(round = TRUE) {
+  rs <- raw_to_scale()
+  out <- lapply(rs, convert_theta, names(rs))
+  bind_dfs(out)
+}
